8534a6ee881fd73cc857b2f1cf26c66a77bf0643
Chan Kang <me@kentakang.com>
2021-10-17 07:42:34 +0900

    feat: add eslint

--- public/app.ts
+++ public/app.ts
@@ -5,45 +5,47 @@ import * as path from 'path';
 let mainWindow: BrowserWindow;
 
 const createWindow = () => {
-    mainWindow = new BrowserWindow({
-        width: 900,
-        height: 400,
-        center: true,
-        resizable: true,
-        fullscreen: false,
-        fullscreenable: true,
-        webPreferences: {
-            nodeIntegration: true,
-            devTools: isDev
-        }
-    });
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 400,
+    center: true,
+    resizable: true,
+    fullscreen: false,
+    fullscreenable: true,
+    webPreferences: {
+      nodeIntegration: true,
+      devTools: isDev,
+    },
+  });
 
-    mainWindow.loadURL(
-        isDev
-        ? 'http://localhost:3000'
-        : `file://${path.join(__dirname, '../build/index.html')}`
-    );
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`,
+  );
 
-    mainWindow.setResizable(true);
-    mainWindow.focus();
+  mainWindow.setResizable(true);
+  mainWindow.focus();
 
-    mainWindow.on('closed', () => (mainWindow = undefined!));
+  mainWindow.on('closed', () => {
+    mainWindow = undefined;
+  });
 
-    if (isDev) {
-        mainWindow.webContents.openDevTools({ mode: 'detach' });
-    }
+  if (isDev) {
+    mainWindow.webContents.openDevTools({ mode: 'detach' });
+  }
 };
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-    if (process.platform === 'darwin') {
-        app.quit();
-    }
+  if (process.platform === 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', () => {
-    if (mainWindow === null) {
-        createWindow();
-    }
-})
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
