d396f1ff1e2bb46759d20566c382016d9f9067fa
Kang Chan <aries.kang@investy.us>
2021-10-28 01:45:46 +0900

    feat: add global style

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable no-use-before-define */
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import { css } from '@linaria/core';
 
 import App from './App';
 
@@ -9,3 +12,13 @@ ReactDOM.render(
   </StrictMode>,
   document.getElementById('root'),
 );
+
+export const globals = css`
+  :global() {
+    html, body, #root {
+      width: 100%;
+      height: 100%;
+      margin: 0;
+    }
+  }
+`;
